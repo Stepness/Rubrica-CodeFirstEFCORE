be23d64b65f379a2afa759b48015980c60479b81
Step <stefanodinucci@gmail.com>
2020-05-27 18:05:52 +0200

    aggiunto servizio

---
+++ Models/Servizi.cs
@@ -0,0 +1,15 @@
+namespace PlayGround_Dependency_Injection.Models
+{
+    public class Servizi : IRubricaServices
+    {
+        public Contatto ottieniContatto()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public ViewModelHome ottieniHome()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using PlayGround_Dependency_Injection.Models;
 
 namespace PlayGround_Dependency_Injection
 {
@@ -24,6 +25,7 @@ namespace PlayGround_Dependency_Injection
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddTransient<IRubricaServices, Servizi>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
