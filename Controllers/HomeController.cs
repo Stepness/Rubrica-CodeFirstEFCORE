cb8285b706a41fb83ec725818edaabdc8e620d22
Step <stefanodinucci@gmail.com>
2020-05-29 11:14:41 +0200

    dotnet run fixed

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -11,8 +11,19 @@ namespace PlayGround_Dependency_Injection.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
 
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
         {
             return View();
         }
